79c8af24ecf88a3287070d097b9964cc08ab66d6
FatemeDallal <dallalfateme2512003@gmail.com>
2024-03-16 21:49:52 -0400

    change models.py in order_module

--- order_module/models.py
+++ order_module/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from account_module.models import User
+from product_module.models import Product
 
-# Create your models here.
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='کاربر')
+    is_paid = models.BooleanField(verbose_name='پرداخت شده / نشده')
+    payment_date = models.DateField(null=True, blank=True, verbose_name='تاریخ پرداخت')
+
+    def __str__(self):
+        return str(self.user)
+
+    def total_price(self):
+        total_amount = 0
+
+        if self.is_paid:
+            for detail_order in self.orderdetail_set:
+                total_amount += detail_order.final_price
+        else:
+            for detail_order in self.orderdetail_set:
+                total_amount += detail_order.count * detail_order.product.price
+
+
+class OrderDetail(models.Model):
+    order = models.ForeignKey('Order', on_delete=models.CASCADE, verbose_name='سفارش')
+    product = models.ForeignKey('Product', on_delete=models.CASCADE, verbose_name='محصول')
+    count = models.IntegerField()
+    final_price = models.IntegerField()
--- software_project/settings.py
+++ software_project/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django_recaptcha',
     'rest_framework',
     'drf_spectacular',
-
+    'order_module',
 ]
 
 MIDDLEWARE = [
